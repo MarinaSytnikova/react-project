fc38e00a31027f9970a6136bf5dacf0d909d068c
Marina <mariwenka100@gmail.com>
2023-09-26 20:28:01 +0300

    Firts

---
+++ 10-condition-rendering/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import PetInfo from './components/PetInfo'
+function App() {
+  return (
+    <div className="App">
+      <PetInfo animal="dog" age="4" hasPet={true} />
+      <PetInfo animal="cat" age="3" hasPet={false} />
+    </div>
+  )
+}
+
+export default App
